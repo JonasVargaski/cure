ee10735dc66f2cdf9567a2714fd653efd8a1989c
Jonas Vargaski <jvargaski@tractian.com>
2024-06-24 20:01:52 -0300

    feat: ota update

--- src/utils/ota_update.h
+++ src/utils/ota_update.h
@@ -15,18 +15,16 @@ bool updateFirmware(String url) {
   HTTPClient http;
   http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
 
-  Serial.println("STARTING OTA UPDATE");
-
   if (http.begin(client, url)) {
     int status = http.GET();
-    log_i("Opening %s got %d.", url.c_str(), status);
+    Serial.printf("[OTA] Opening %s got %d\n", url.c_str(), status);
     size_t size = http.getSize();
-    log_i("http size: %d. ", size);
-    log_i("Will access Stream:, free heap %dkb", ESP.getFreeHeap() / 1024);
+
     if (status == HTTP_CODE_OK) {
       Update.begin();
       Update.onProgress([size](size_t pos, size_t all) {
-        Serial.println("Progress: " + String(pos) + " of " + String(all));
+        float percentage = (float)pos / (float)all * 100.0;
+        Serial.println("[OTA] Progress: " + String(percentage, 2) + "% | " + String(pos) + " of " + String(all));
       });
 
       Stream& stream = http.getStream();
@@ -37,22 +35,22 @@ bool updateFirmware(String url) {
         Update.write(buffer, read);
         written += read;
       }
-      log_i("Got %d bytes", written);
+      Serial.printf("[OTA] Got %d bytes\n", written);
     } else {
-      log_e("Got http status: %d", status);
+      Serial.printf("[OTA] Error: http status: %d\n", status);
       http.end();
       return false;
     }
   }
-  http.end();
-  if (Update.end(true)) {  // true to set the size to the current progress
-    Serial.println("SUCCESS");
 
-    // Suppress the ota Firmware switch!
+  http.end();
+  if (Update.end(true)) {
     const esp_partition_t* running = esp_ota_get_running_partition();
     esp_ota_set_boot_partition(running);
     success = true;
+    Serial.println(F("[OTA] Update Success"));
   } else {
+    Serial.print(F("[OTA] Update error"));
     Serial.println(Update.errorString());
   }
   return success;
