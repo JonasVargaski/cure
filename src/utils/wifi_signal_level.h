228874988ae025b94de249edb8d657800c0bb145
Jonas Vargaski <jvargaski@tractian.com>
2024-06-26 20:56:35 -0300

    fix: wifi signal level

--- src/utils/wifi_signal_level.h
+++ src/utils/wifi_signal_level.h
@@ -6,13 +6,13 @@ unsigned int parseSignalLevel(int rssi) {
   if (rssi <= -100)
     return 0;
 
-  if (rssi <= -80)
+  if (rssi <= -85)
     return 1;
 
-  if (rssi <= -60)
+  if (rssi <= -75)
     return 2;
 
-  if (rssi <= -40)
+  if (rssi <= -60)
     return 3;
 
   return 4;
